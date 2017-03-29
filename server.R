4449b17dc8b7c1fe3a03470d77a5be53294fc0f1
Grant Humphries <Grant Humphries>
2017-03-29 09:57:43 +0100

    fixed time stamp issues and constructed docx

--- server.R
+++ server.R
@@ -7,17 +7,33 @@
 
 library(shiny)
 library(xtable)
+library(readxl)
+library(dplyr)
 
-X <- read.csv('AbstractsV2.csv')
+X <- readRDS("Abstracts.rds")
+Y <- readRDS("Schedule.rds")
+#X <- read.csv('AbstractsV2.csv')
 
-X$ids <- iconv(paste(as.character(X$Last),', ',as.character(X$First),sep=''),'latin1','UTF-8')
-X$Affiliation <- iconv(as.character(X$Affiliation),'latin1','UTF-8')
-X$Country <- iconv(as.character(X$Country),'latin1','UTF-8')
-X$First <- iconv(as.character(X$First),'latin1','UTF-8')
-X$Last <- iconv(as.character(X$Last),'latin1','UTF-8')
-X$Title <- iconv(as.character(X$Title),'latin1','UTF-8')
-X$Abstract <- iconv(as.character(X$Abstract),'latin1','UTF-8')
-X$Topic <- iconv(as.character(X$Topic),'latin1','UTF-8')
+X$ids <- paste(as.character(X$Last),', ',as.character(X$First),sep='')
+X$Affiliation <- as.character(X$Affiliation)
+X$Country <- as.character(X$Country)
+X$First <- as.character(X$First)
+X$Last <- as.character(X$Last)
+X$Title <- as.character(X$Title)
+X$Abstract <- as.character(X$Abstract)
+X$Topic <- as.character(X$Topic)
+
+
+X <- X[order(X$Last),]
+
+
+
+
+Y$fullname <- paste(Y$First, Y$Last, sep=" ")
+Y$day <- paste("April", format(strptime(Y$Date,format="%Y-%m-%d"),"%d"), sep=" ")
+Y$Time<- round(Y$Time,units='min')
+Y$UTC <- format(Y$Time,"%H:%M:%S")
+
 
 shinyServer(function(input, output, session) {
 
@@ -95,6 +111,24 @@ shinyServer(function(input, output, session) {
     }
   })
   
+  output$Time <- renderTable({
+    Dat <- X[which(X$ids == input$Presenter),]
+    UTCtime <- round(strptime(as.character(Dat$DateTimeUTC),format="%Y-%m-%d %H:%M:%S"),units='min')
+    Localtime <- round(strptime(as.character(Dat$LocalTimeUTC),format="%Y-%m-%d %H:%M:%S"),units='min')
+    Dat$UTCTm <- paste("April ",as.character(UTCtime$mday)," ",format(UTCtime, "%H:%M"),sep="")
+    Dat$LOCTm <- paste("April ",as.character(Localtime$mday)," ",format(Localtime, "%H:%M"),sep="")
+    
+    if(nrow(Dat) > 1){
+      Dat$lab <- c(1:nrow(Dat))
+      Tab <- data.frame(Label = Dat$lab, UTC = Dat$UTCTm, "Local Timezone" = Dat$LOCTm, "Offset From UTC" = Dat$Timezone, check.names=FALSE)
+    }else{
+      Tab <- data.frame(UTC = Dat$UTCTm, "Local Timezone" = Dat$LOCTm, "Offset From UTC" = Dat$Timezone, check.names=FALSE)
+    }
+    
+    
+  })
+  
+  
   
   output$TitleDat <- renderUI({
     Dat <- X[which(X$ids == input$Presenter),]
@@ -121,6 +155,50 @@ shinyServer(function(input, output, session) {
     HTML(Abshtml)
 
   })
+  
+  
+  ########## For the schedule
+  
+  observeEvent(input$filter2, {
+    if(input$filter2 == 'Day'){
+      shinyjs::enable('Day')
+      shinyjs::disable('Session')
+      
+    }else if(input$filter2 == 'Session'){
+      shinyjs::enable('Session')
+      shinyjs::disable('Day')
+      
+    }else if(input$filter2 == 'None'){
+      shinyjs::disable('Session')
+      shinyjs::disable("Day")
+    }
+  })
+  
+  
+  output$Schedule <- renderTable(striped = TRUE,hover=TRUE, expr= {
+    Dat <- data.frame(Name = Y$fullname, Handle = Y$Handle, Day = Y$day, UTC = Y$UTC,Topic = Y$Topic, Title = Y$Title,check.names=FALSE)
+    
+    Dat <- tbl_df(Dat) %>% filter(!is.na(Title))
+    
+    
+    if(input$filter2 == 'Session'){
+      Dat <- tbl_df(Dat)
+      Dat <- Dat %>% filter(Topic == input$Session)
+      Dat <- data.frame(Dat)
+    }else if(input$filter2 == "Day"){
+      Dat <- tbl_df(Dat)
+      Dat <- Dat %>% filter(Day == input$Day)
+      Dat <- data.frame(Dat)
+    }else if(input$filter == "None"){
+      Dat <- data.frame(Dat)
+    }
+    
+    
+    
+    
+  })
+  
+  
   
 
 })
--- ui.R
+++ ui.R
@@ -9,14 +9,18 @@ library(shiny)
 library(shinythemes)
 library(xtable)
 library(shinyjs)
+library(dplyr)
+library(readxl)
 
 
-X <- read.csv('AbstractsV2.csv')
+#X <- read_excel("Schedule_FINAL.xlsx", sheet=3)[1:128,]
+#Y <- tbl_df(read_excel("Schedule_FINAL.xlsx",sheet=1)[1:284,1:10])
+X <- readRDS("Abstracts.rds")
+Y <- tbl_df(readRDS("Schedule.rds"))
 
-
-X$ids <-iconv(paste(as.character(X$Last),', ',as.character(X$First),sep=''),'latin1','UTF-8')
-X$Affiliation <- iconv(as.character(X$Affiliation),'latin1','UTF-8')
-X$Country <- iconv(as.character(X$Country),'latin1','UTF-8')
+X$ids <-paste(as.character(X$Last),', ',as.character(X$First),sep='')
+X$Affiliation <- as.character(X$Affiliation)
+X$Country <- as.character(X$Country)
 
 
 shinyUI(
@@ -24,56 +28,111 @@ shinyUI(
     title = img(src="Logo.png",width='200px'),
     windowTitle = 'WSTC3 Abstracts',
     theme=shinytheme("spacelab"),
-
+    selected = "Home",
     tags$head(
       tags$style(HTML("@import url('//fonts.googleapis.com/css?family=Roboto:100,500');")),
       tags$style(type="text/css", ".navbar {height: 90px;}")
       ),
-                   
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      useShinyjs(), 
-        selectInput("Presenter", label = "Presenter:",
-                    choices = c(as.character(X$ids))),
-        
-        hr(),
-        
-        h3('Filter presenter by:'),
+    
+    tabPanel("Home",
+             mainPanel(
+               headerPanel(
+                 h1("Welcome to the Interactive WSTC3 Abstract Guide", style="font-family:Roboto; font-weight:100; text-align:center")
+               ),
+               hr(),
+               h2("Click the above links in the header to either SEARCH BY PRESENTER, or VIEW THE SCHEDULE"),
+               HTML("<p> You can also check out <a href='http://www.seabirds.net'>Seabirds.net</a> for more information ")
+             )
+             
+             ),
+    tabPanel("Search by Presenter",
+      # Sidebar with a slider input for number of bins
+        sidebarLayout(
+          sidebarPanel(
+            useShinyjs(), 
+              selectInput("Presenter", label = "Presenter:",
+                          choices = c(as.character(X$ids))),
+              
+              hr(),
+              
+              h3('Filter presenter by:'),
+            
+              radioButtons("filter", label = NULL, choices = c("Country", "Affiliation","Topic", "None"), inline=TRUE, selected='None'),
+            
+            
+              selectInput("Country", label = "Country:", selected = 'Australia ',
+                          choices = c(sort(as.character(X$Country)))),
+              
+              
+              selectInput("Affiliation", label = "Affiliation:",
+                          choices = c(sort(as.character(X$Affiliation)))),
+            
+              selectInput("Topic", label = "Topic:",
+                        choices = c(sort(unique(as.character(X$Topic))))),
+            
+              hr(),
+              img(src='Blackbawks.png',width = '25%'),
+              HTML('<span>Created by: <a href="http://blackbawks.net">Black bawks data science ltd.</a></span>'),
+              HTML('<p><a href="mailto:grwhumphries@blackbawks.net">Click here</a> to report a bug</p>')
+              
+            
+            
+              
+          ),
       
-        radioButtons("filter", label = NULL, choices = c("Country", "Affiliation","Topic", "None"), inline=TRUE, selected='None'),
+          # Show a plot of the generated distribution
+          mainPanel(
+            headerPanel(
+              h1("Interactive WSTC3 Abstract Guide", style="font-family:Roboto; font-weight:100")
+            ),
+            tableOutput('Person'),
+            tableOutput('Info'),
+            tableOutput('Time'),
+            HTML('<h2> Title </h2>'),
+            htmlOutput('TitleDat'),
+            HTML('<h2> Abstract </h2>'),
+            htmlOutput('AbstractDat'),
+            hr()
+          )
       
-      
-        selectInput("Country", label = "Country:", selected = 'Australia ',
-                    choices = c(sort(as.character(X$Country)))),
-        
-        
-        selectInput("Affiliation", label = "Affiliation:",
-                    choices = c(sort(as.character(X$Affiliation)))),
-      
-        selectInput("Topic", label = "Topic:",
-                  choices = c(sort(unique(as.character(X$Topic))))),
-      
-        hr(),
-        img(src='Blackbawks.png',width = '25%'),
-        HTML('<span>Created by: <a href="http://blackbawks.net">Black bawks data science ltd.</a></span>'),
-        HTML('<p><a href="mailto:grwhumphries@blackbawks.net">Click here</a> to report a bug</p>')
-        
-        
-    ),
+        )
+    ),  ## tabPanel END
 
-    # Show a plot of the generated distribution
-    mainPanel(
-      headerPanel(
-        h1("Interactive WSTC3 Abstract Guide", style="font-family:Roboto; font-weight:100")
-      ),
-      tableOutput('Person'),
-      tableOutput('Info'),
-      HTML('<h2> Title </h2>'),
-      htmlOutput('TitleDat'),
-      HTML('<h2> Abstract </h2>'),
-      htmlOutput('AbstractDat'),
-      hr()
-    )
-  )
+    tabPanel("Schedule",
+             
+             sidebarLayout(
+               sidebarPanel(
+                 useShinyjs(), 
+                 h3('Filter by:'),
+                 
+                 radioButtons("filter2", label = NULL, choices = c("Day","Session","None"), inline=TRUE, selected='None'),
+                 
+                 
+                 selectInput("Day", label = "Day:", selected = 'April 12 ',
+                             choices = c('April 12','April 13', 'April 14')),
+                 
+                 selectInput("Session", label = "Session:", selected = 'Climate',
+                             choices = c(sort(as.character(Y$Topic)))),
+                 
+
+                 hr(),
+                 img(src='Blackbawks.png',width = '25%'),
+                 HTML('<span>Created by: <a href="http://blackbawks.net">Black bawks data science ltd.</a></span>'),
+                 HTML('<p><a href="mailto:grwhumphries@blackbawks.net">Click here</a> to report a bug</p>')
+                 
+                 
+               ),
+               
+               # Show a plot of the generated distribution
+               mainPanel(
+                 headerPanel(
+                   h1("Interactive WSTC3 Abstract Guide", style="font-family:Roboto; font-weight:100")
+                 ),
+                 tableOutput("Schedule")
+               )
+               
+             )
+             
+             )
+    
 ))
